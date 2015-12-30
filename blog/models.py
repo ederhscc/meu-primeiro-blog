f9ab70850ee66388b16832f4006fafe719204f27
ederhscc <ederhscc@gmail.com>
2015-12-30 14:23:52 -0200

    My Django Eder app, primeiro commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    text = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_publicacao = models.DateTimeField(blank=True, null=True)
+	
+    def publicacao(self):
+	    self.data_publicacao = timezone.now()
+	    self.save()
+	
+    def __str__(self):
+	    return self.titulo
+
+	
+
+
